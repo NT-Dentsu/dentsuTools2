3f1acb7991a38cc3463acf8218e7f6dbe34093eb
towa1204 <lsadsfj@gmail.com>
2022-07-23 02:37:36 +0900

    php-cs-fixerの導入

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,34 @@
+<?php
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PhpCsFixer' => true,
+        'blank_line_before_statement' => [
+            'statements' => [
+                'for', 'foreach', 'if', 'switch', 'try'
+            ],
+        ],
+        'final_static_access' => true,
+        'global_namespace_import' => true, 
+        'phpdoc_add_missing_param_annotation' => [
+            'only_untyped' => false
+        ],
+        'yoda_style' => [
+            'always_move_variable' => false,
+            'equal' => false, 
+            'identical' => false
+        ],
+    ])
+    ->setFinder(PhpCsFixer\Finder::create()
+        ->exclude('vendor')
+        ->in([
+            __DIR__ . '/app',
+            __DIR__ . '/config',
+            __DIR__ . '/database',
+            __DIR__ . '/routes',
+            __DIR__ . '/resources/views',
+            __DIR__ . '/tests',
+        ])
+    )
+    ;
