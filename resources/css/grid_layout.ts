3f632f06634b3a585004570afa8e0c9cd3c4aee8
NumLock810 <kutsuhimo197@gmail.com>
2022-09-10 14:14:06 +0900

    grid_layout.cssをtypescript+reactで書き直し

---
+++ resources/css/grid_layout.ts
@@ -0,0 +1,93 @@
+/**
+ * 制作者：NumLock
+ * 制作日：2022/09/10
+ * 更新日：
+ * グリッドレイアウト用のcssをstyled-componentsで書きたいやつ
+ */
+
+// import React from 'react';
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    position: relative;
+    max-width: 1980px;
+    margin: 0 auto; /*全体の中央寄せ*/
+`;
+
+export const Container = styled.div`
+    width: 100%;
+    margin: 0 auto; /*中央寄せ*/
+    position: relative;
+
+    /* グリッドエリアを正方形にする */
+    /* padding-bottom: 100%; */
+
+    display: grid; /* グリッドレイアウト */
+
+    /* 4*4のグリッドを定義 */
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+
+    /* グリッド間の間隔 */
+    gap: 1.25%;
+`;
+
+// パネル用
+
+// propsのためのtype
+type Area = {
+    area: string;
+};
+// panelを正方形にする用
+export const PanelWrap = styled.div`
+    width: 100%;
+    /* padding-bottom: 100%; */
+    margin: 0px;
+    z-index: 10;
+    /* grid-areaをpropで指定 */
+    grid-area: ${(props: Area) => props.area};
+
+    &::before {
+        content: '';
+        display: block;
+        padding-top: 100%;
+    }
+`;
+
+// panel自体
+const Panel = styled.div`
+    background-color: aqua;
+    /* padding内を塗りつぶし */
+    background-clip: content-box;
+    /* margin: 10px; */
+    padding: 0px;
+    margin: 0px;
+    /* border: 1px solid #eee; */
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+    z-index: 20;
+`;
+
+export const PanelS = styled(Panel)`
+    background-color: darkblue;
+`;
+
+export const PanelM = styled(Panel)`
+    background-color: brown;
+`;
+
+export const PanelL = styled(Panel)`
+    background-color: darkgreen;
+`;
+
+// 画像周りの設定
+export const PanelImg = styled.img`
+    /* position: absolute; */
+    width: 100%;
+    height: 100%;
+
+    /* object-fitでトリミング */
+    object-fit: cover;
+`;
