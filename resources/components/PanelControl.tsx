5474108ba4f5010f6858ccf1ee840da8fce9b89f
NumLock810 <kutsuhimo197@gmail.com>
2022-09-17 19:50:39 +0900

    div_control.jsをtsで書き直し forEeachの関数だったものを変数受け取ってjsx返す形式に変更

---
+++ resources/components/PanelControl.tsx
@@ -0,0 +1,78 @@
+/**
+ * 制作者：NumLock
+ * 制作日：2022/09/10
+ * 更新日：2022/09/17
+ * panelの追加，変更など
+ */
+
+import React, { Fragment } from 'react';
+// import usePanelInfo from './PanelInfo';
+import * as GridLayout from '../css/grid_layout';
+
+// Infoの型宣言
+type Info = Array<{
+    className: string;
+    gridSize: string;
+    toolLink: string;
+    imageLink: string;
+}>;
+
+// containerを取得
+// const cont = document.getElementById(GridLayout.Container)!;
+
+// containerの子要素を更新する
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+const ContainerUpdate = (info: Info, flag: boolean) => {
+    // サイズ調整とか
+    const panelAdjust = (
+        <>
+            {/* グリッドのサイズ調整用 */}
+            <GridLayout.PanelWrap area="1 / 1" name="1" />
+            <GridLayout.PanelWrap area="1 / 2" name="1" />
+            <GridLayout.PanelWrap area="1 / 3" name="1" />
+            <GridLayout.PanelWrap area="1 / 4" name="1" />
+            <GridLayout.PanelWrap area="2 / 1" name="1" />
+            <GridLayout.PanelWrap area="2 / 2" name="1" />
+            <GridLayout.PanelWrap area="2 / 3" name="1" />
+            <GridLayout.PanelWrap area="2 / 4" name="1" />
+            <GridLayout.PanelWrap area="3 / 1" name="1" />
+            <GridLayout.PanelWrap area="3 / 2" name="1" />
+            <GridLayout.PanelWrap area="3 / 3" name="1" />
+            <GridLayout.PanelWrap area="4 / 1" name="1" />
+            <GridLayout.PanelWrap area="4 / 2" name="1" />
+            <GridLayout.PanelWrap area="4 / 3" name="1" />
+            <GridLayout.PanelWrap area="4 / 4" name="1" />
+            {/* ここにパネルが追加される */}
+        </>
+    );
+
+    // フラグによってコンテンツリンクの有無を変更
+
+    // map使用
+    // eslint-disable-next-line react/destructuring-assignment
+    const panel = info.map((data) => (
+        <div
+            className={data.className}
+            // 三項演算子
+            style={flag ? { gridArea: data.gridSize } : { gridArea: data.gridSize, zIndex: 5 }}
+        >
+            {/* 三項演算子 */}
+            {flag ? (
+                <a href={data.toolLink}>
+                    <img src={data.imageLink} alt="" />
+                </a>
+            ) : (
+                <img src={data.imageLink} alt="" />
+            )}
+        </div>
+    ));
+
+    // 子要素追加(できてたらいいなぁ)
+    // ReactDOM.createPortal(panel, cont);
+    return (
+        <GridLayout.Container>
+            {panelAdjust}
+            {panel}
+        </GridLayout.Container>
+    );
+};
