080a47b81a367d61e9f4eced9bce7fad077b4925
towa1204 <lsadsfj@gmail.com>
2022-09-23 20:47:50 +0900

    md以上のみサイドバーを表示するよう設定

--- resources/layout/Layout.tsx
+++ resources/layout/Layout.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import SideBar from './SideBar';
 
 const Layout = () => (
-    <div>
-        <div>
+    <>
+        <header>
             <h1 style={{ textAlign: 'center' }}>タイトルバー</h1>
-        </div>
-        <SideBar />
-        <Outlet />
-    </div>
+        </header>
+        <Container fluid>
+            <Row className="flex-xl-nowrap">
+                <Col xs={12} md={3} xl={4}>
+                    <SideBar />
+                </Col>
+                <Col as="main" xs={12} md={9} xl={8}>
+                    <Outlet />
+                </Col>
+            </Row>
+        </Container>
+    </>
 );
 
 export default Layout;
--- resources/layout/SideBar.tsx
+++ resources/layout/SideBar.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import * as Icon from 'react-feather';
 
 const SideBar = () => (
-    <div>
-        <h2>サイドバー</h2>
-        <ul>
-            <li>
-                <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-                <NavLink to="/profile">Profile</NavLink>
-            </li>
-            <li>
-                <NavLink to="/customize">Customize</NavLink>
-            </li>
-            <li>
-                <NavLink to="/signin">Signin</NavLink>
-            </li>
-        </ul>
-    </div>
+    <Nav className="d-flex flex-column d-none d-md-flex">
+        <Nav.Item>
+            <NavLink to="/">
+                <Icon.Home /> Home
+            </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+            <NavLink to="/profile">
+                <Icon.User /> Profile
+            </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+            <NavLink to="/customize">
+                <Icon.Layout /> Customize
+            </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+            <NavLink to="#">
+                <Icon.File /> Contents
+            </NavLink>
+        </Nav.Item>
+    </Nav>
 );
 
 export default SideBar;
