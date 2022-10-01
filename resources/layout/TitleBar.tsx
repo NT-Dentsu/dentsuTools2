05727e28c5173db271c32cbcad1d2a41ac564d53
towa1204 <lsadsfj@gmail.com>
2022-10-01 16:59:55 +0900

    タイトルバーのユーザアイコン部分の実装

--- resources/layout/TitleBar.tsx
+++ resources/layout/TitleBar.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import styled from 'styled-components';
+
+const NavIcon = styled(Nav.Link).attrs({
+    as: NavLink,
+    to: '/signin',
+})`
+    img {
+        width: 65px;
+        height: 65px;
+        border-radius: 50%;
+        background: skyblue;
+        /* 配置時に上下の幅を持たせる */
+        margin-top: 5%;
+        margin-bottom: 5%;
+    }
+`;
 
 const TitleBar = () => (
     <Navbar as="header" bg="dark" variant="dark" sticky="top">
@@ -9,6 +26,13 @@ const TitleBar = () => (
             <Navbar.Brand as={NavLink} to="/">
                 NT-dentsu
             </Navbar.Brand>
+            <Nav className="justify-content-end">
+                <Nav.Item className="user-icon">
+                    <NavIcon>
+                        <img src="/assets/default_icon.jpg" />
+                    </NavIcon>
+                </Nav.Item>
+            </Nav>
         </Container>
     </Navbar>
 );
