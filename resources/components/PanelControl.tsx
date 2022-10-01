a1653b15846858a69f4073a0c91dd7f49c7a5e91
NumLock810 <kutsuhimo197@gmail.com>
2022-10-01 18:28:09 +0900

    name属性の値修正

--- resources/components/PanelControl.tsx
+++ resources/components/PanelControl.tsx
@@ -1,7 +1,7 @@
 /**
  * 制作者：NumLock
  * 制作日：2022/09/10
- * 更新日：2022/09/17
+ * 更新日：2022/10/01
  * panelの追加，変更など
  */
 
@@ -27,20 +27,21 @@ const ContainerUpdate = (info: Info, flag: boolean) => {
         <>
             {/* グリッドのサイズ調整用 */}
             <GridLayout.PanelWrap area="1 / 1" name="1" />
-            <GridLayout.PanelWrap area="1 / 2" name="1" />
-            <GridLayout.PanelWrap area="1 / 3" name="1" />
-            <GridLayout.PanelWrap area="1 / 4" name="1" />
-            <GridLayout.PanelWrap area="2 / 1" name="1" />
-            <GridLayout.PanelWrap area="2 / 2" name="1" />
-            <GridLayout.PanelWrap area="2 / 3" name="1" />
-            <GridLayout.PanelWrap area="2 / 4" name="1" />
-            <GridLayout.PanelWrap area="3 / 1" name="1" />
-            <GridLayout.PanelWrap area="3 / 2" name="1" />
-            <GridLayout.PanelWrap area="3 / 3" name="1" />
-            <GridLayout.PanelWrap area="4 / 1" name="1" />
-            <GridLayout.PanelWrap area="4 / 2" name="1" />
-            <GridLayout.PanelWrap area="4 / 3" name="1" />
-            <GridLayout.PanelWrap area="4 / 4" name="1" />
+            <GridLayout.PanelWrap area="1 / 2" name="2" />
+            <GridLayout.PanelWrap area="1 / 3" name="3" />
+            <GridLayout.PanelWrap area="1 / 4" name="4" />
+            <GridLayout.PanelWrap area="2 / 1" name="5" />
+            <GridLayout.PanelWrap area="2 / 2" name="6" />
+            <GridLayout.PanelWrap area="2 / 3" name="7" />
+            <GridLayout.PanelWrap area="2 / 4" name="8" />
+            <GridLayout.PanelWrap area="3 / 1" name="9" />
+            <GridLayout.PanelWrap area="3 / 2" name="10" />
+            <GridLayout.PanelWrap area="3 / 3" name="11" />
+            <GridLayout.PanelWrap area="3 / 4" name="12" />
+            <GridLayout.PanelWrap area="4 / 1" name="13" />
+            <GridLayout.PanelWrap area="4 / 2" name="14" />
+            <GridLayout.PanelWrap area="4 / 3" name="15" />
+            <GridLayout.PanelWrap area="4 / 4" name="16" />
             {/* ここにパネルが追加される */}
         </>
     );
