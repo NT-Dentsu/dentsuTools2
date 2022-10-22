e2a47aaa7792ed9e8437174e30129b623417679a
towa1204 <lsadsfj@gmail.com>
2022-10-23 04:05:03 +0900

    useEffectで初回レンダー時にauthcheckを叩く

--- resources/route.tsx
+++ resources/route.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import axios, { AxiosError } from 'axios';
 import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
 import Customize from './pages/customize/Customize';
@@ -8,19 +9,35 @@ import SignIn from './pages/auth/SignIn';
 import SignUp from './pages/auth/SignUp';
 import Layout from './layout/Layout';
 
-const App = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/customize" element={<Customize />} />
-            </Route>
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-        </Routes>
-    </BrowserRouter>
-);
+type MessageResponse = {
+    message: string;
+};
+
+const App = () => {
+    useEffect(() => {
+        axios
+            .get<MessageResponse>('/api/authcheck')
+            .then((response) => {
+                alert(response.data.message);
+            })
+            .catch((error: AxiosError<MessageResponse>) => {
+                if (error.response !== undefined) alert(error.response.data.message);
+            });
+    }, []);
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/customize" element={<Customize />} />
+                </Route>
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 const element = document.getElementById('app');
 if (element === null) {
