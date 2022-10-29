3ea90fbacfb50a2d1e84b8be1e18cda236a8b706
NumLock810 <kutsuhimo197@gmail.com>
2022-10-29 17:56:03 +0900

    コメントアウト解除と最後までリファクタリング

--- resources/css/Dashboard.ts
+++ resources/css/Dashboard.ts
@@ -1,75 +1,75 @@
 /**
  * 制作者：NumLock
  * 制作日：2022/10/22
- * 更新日：
+ * 更新日：2022/10/29
  * 説明：dashboard.cssをsyled-compornentsで書き直し
  */
 
 // 要らない気がするので保留
 // やっぱりいる
 
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-// export const Body = styled.div`
-//     font-size: 0.875rem;
-// `;
+export const Body = styled.div`
+    font-size: 0.875rem;
+`;
 
-// export const Feather = styled.div`
-//     width: 16px;
-//     height: 16px;
-//     vertical-align: text-bottom;
-// `;
+export const Feather = styled.div`
+    width: 16px;
+    height: 16px;
+    vertical-align: text-bottom;
+`;
 
-// /*
-// * サイドバー
-// */
-// export const Sidebar = styled.nav`
-//     position: fixed;
-//     top: 0;
-//     bottom: 0;
-//     left: 0;
-//     z-index: 100;
-//     padding: 48px 0 0;
-//     box-shadow: inset -1px 0 0 rgba(0, 0, 0, 0.1);
-// `;
+/*
+ * サイドバー
+ */
+export const Sidebar = styled.nav`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 100;
+    padding: 48px 0 0;
+    box-shadow: inset -1px 0 0 rgba(0, 0, 0, 0.1);
+`;
 
-// /* スクロール時の張り付き設定 */
-// export const SidebarSticky = styled.div`
-//     position: sticky;
-//     top: 4rem;
-//     padding-top: 2rem;
-//     overflow-x: hidden;
-//     overflow-y: auto;
-// `
+/* スクロール時の張り付き設定 */
+export const SidebarSticky = styled.div`
+    position: sticky;
+    top: 4rem;
+    padding-top: 2rem;
+    overflow-x: hidden;
+    overflow-y: auto;
+`;
 
-// /* サイドバーに表示する要素のスタイル */
-// export const NavLink = styled(Sidebar)`
-//     font-weight: 500;
-//     color: #333;
-// `
+/* サイドバーに表示する要素のスタイル */
+export const NavLink = styled(Sidebar.withComponent('a'))`
+    font-weight: 500;
+    color: #333;
+    &.active {
+        color: #007bff;
+    }
+    &.active .feather {
+        color: inherit;
+    }
+    &:hover .feather {
+        color: inherit;
+    }
+`;
 
-// export const Active = styled(NavLink)`
-//     color: #007bff;
-// `
+/* サイドバーに表示する要素のアイコン */
+export const NavLinkFeather = styled(NavLink.withComponent('div'))`
+    margin-right: 4px;
+    color: #999;
+`;
 
-// /* サイドバーに表示する要素のアイコン */
-// export const Feather = styled(NavLink)`
-//     margin-right: 4px;
-//     color: #999;
-// `
-
-// .sidebar .nav-link:hover .feather,
-// .sidebar .nav-link.active .feather {
-//     color: inherit;
-// }
-
-// /*
-// * タイトルバー
-// */
-// .navbar .navbar-brand {
-//     padding-top: 0.8%;
-//     padding-bottom: 0.8%;
-//     font-size: 28px;
-//     background-color: rgba(0, 0, 0, .25);
-//     box-shadow: inset -1px 0 0 rgba(0, 0, 0, .25);
-// }
+/*
+ * タイトルバー
+ */
+export const NavbarBrand = styled.a`
+    padding-top: 0.8%;
+    padding-bottom: 0.8%;
+    font-size: 28px;
+    background-color: rgba(0, 0, 0, 0.25);
+    box-shadow: inset -1px 0 0 rgba(0, 0, 0, 0.25);
+`;
