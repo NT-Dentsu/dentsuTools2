12791fe825bfbde0243573ebcc5d4ab41ede03fe
towa1204 <lsadsfj@gmail.com>
2022-11-12 05:09:44 +0900

    seederのユーザ作成をcreateAccountに変更

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
         $this->call(PresetSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,7 +12,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create(['user_id' => 'helloworld', 'user_name' => 'helloworld', 'password_hash' => Hash::make('helloworld')]);
-        User::create(['user_id' => 'test-user', 'user_name' => 'test-user', 'password_hash' => Hash::make('test-password')]);
+        User::createAccount('helloworld', 'helloworld');
+        User::createAccount('test-user', 'test-password');
     }
 }
