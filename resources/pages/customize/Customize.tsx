d238cb9e567cef54f18beb27256d8a89d7dedf6d
NumLock810 <kutsuhimo197@gmail.com>
2022-11-19 16:45:13 +0900

    パネルプレビュー追加

--- resources/pages/customize/Customize.tsx
+++ resources/pages/customize/Customize.tsx
@@ -6,7 +6,49 @@
  */
 
 import React from 'react';
+import * as Cust from './css/Customize';
+import * as GridLayout from '../../css/GridLayout';
 
-const Customize = () => <p>カスタマイズ画面です。</p>;
+const Customize = () => {
+    const hoge = () => {
+        console.log('hoge');
+    };
+
+    return (
+        <>
+            {hoge()}
+            {/* ここからカスタマイズ用の諸々  */}
+            {/* 横並びにするために全体をラップ */}
+            <Cust.CustomizeFlex>
+                <Cust.Preview>
+                    <h3>Now Layout</h3>
+                    {/* 「id=”container”」という親要素（div 要素）内に配置した「class=”panel_wrap”」という要素（div 要素）を整列させる処理を行う */}
+                    <GridLayout.Wrapper>
+                        <GridLayout.Container>
+                            {/* グリッドのサイズ調整用 */}
+                            <GridLayout.PanelWrap area="1 / 1" name="0" />
+                            <GridLayout.PanelWrap area="1 / 2" name="1" />
+                            <GridLayout.PanelWrap area="1 / 3" name="2" />
+                            <GridLayout.PanelWrap area="1 / 4" name="3" />
+                            <GridLayout.PanelWrap area="2 / 1" name="4" />
+                            <GridLayout.PanelWrap area="2 / 2" name="5" />
+                            <GridLayout.PanelWrap area="2 / 3" name="6" />
+                            <GridLayout.PanelWrap area="2 / 4" name="7" />
+                            <GridLayout.PanelWrap area="3 / 1" name="8" />
+                            <GridLayout.PanelWrap area="3 / 2" name="9" />
+                            <GridLayout.PanelWrap area="3 / 3" name="10" />
+                            <GridLayout.PanelWrap area="3 / 4" name="11" />
+                            <GridLayout.PanelWrap area="4 / 1" name="12" />
+                            <GridLayout.PanelWrap area="4 / 2" name="13" />
+                            <GridLayout.PanelWrap area="4 / 3" name="14" />
+                            <GridLayout.PanelWrap area="4 / 4" name="15" />
+                            {/* ここにパネルが追加される */}
+                        </GridLayout.Container>
+                    </GridLayout.Wrapper>
+                </Cust.Preview>
+            </Cust.CustomizeFlex>
+        </>
+    );
+};
 
 export default Customize;
