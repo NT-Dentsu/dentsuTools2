5eebce8b8a9e539d7500624acdbd6d0c300942b5
towa1204 <lsadsfj@gmail.com>
2022-12-10 17:38:49 +0900

    updatePanelsのステータスコードを確認するテストの作成

--- tests/Feature/PanelsTest.php
+++ tests/Feature/PanelsTest.php
@@ -153,6 +153,72 @@ class PanelsTest extends TestCase
         ],
     ];
 
+    private $preset002Panels = [
+        'layout' => [
+            [
+                'panel_name' => 'size S',
+                'anchor_num' => 0,
+                'panel_size' => 5,
+            ],
+            [
+                'panel_name' => 'size M hol',
+                'anchor_num' => 1,
+                'panel_size' => 4,
+            ],
+            [
+                'panel_name' => 'size S',
+                'anchor_num' => 3,
+                'panel_size' => 5,
+            ],
+            [
+                'panel_name' => 'size M var',
+                'anchor_num' => 4,
+                'panel_size' => 3,
+            ],
+            [
+                'panel_name' => 'size L',
+                'anchor_num' => 5,
+                'panel_size' => 2,
+            ],
+            [
+                'panel_name' => 'size M var',
+                'anchor_num' => 7,
+                'panel_size' => 3,
+            ],
+            [
+                'panel_name' => 'size S',
+                'anchor_num' => 12,
+                'panel_size' => 5,
+            ],
+            [
+                'panel_name' => 'size M hol',
+                'anchor_num' => 13,
+                'panel_size' => 4,
+            ],
+            [
+                'panel_name' => 'size S',
+                'anchor_num' => 15,
+                'panel_size' => 5,
+            ],
+        ],
+    ];
+
+    // 重なってしまうパネル
+    private $superimposedPanels = [
+        'layout' => [
+            [
+                'panel_name' => 'size S',
+                'anchor_num' => 0,
+                'panel_size' => 5,
+            ],
+            [
+                'panel_name' => 'size L',
+                'anchor_num' => 1,
+                'panel_size' => 5,
+            ],
+        ],
+    ];
+
     // シーダの実行
     public function setUp(): void
     {
@@ -208,7 +274,97 @@ class PanelsTest extends TestCase
     public function testUpdatePanels()
     {
         $user = User::createAccount('sample003', 'password003');
-        $response = $this->actingAs($user)->getJson('/api/panels');
+
+        $response = $this->postJson('/api/panels');
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+
+        // パネル更新が成功するケース1 preset002に更新
+        $response = $this->actingAs($user)->postJson('/api/panels', $this->preset002Panels);
         $response->assertStatus(Response::HTTP_OK);
+
+        // パネル更新が成功するケース2
+        $changedPreset002panels = $this->preset002Panels;
+        $changedPreset002panels['layout'][4] = [
+            'panel_name' => 'size S',
+            'anchor_num' => 5,
+            'panel_size' => 5,
+        ];
+        $response = $this->actingAs($user)->postJson('/api/panels', $changedPreset002panels);
+        $response->assertStatus(Response::HTTP_OK);
+
+        // パネル更新が成功するケース3 アンカー番号が重複するケース
+        $changedPreset002panels = $this->preset002Panels;
+        $changedPreset002panels['layout'][1] = [
+            'panel_name' => 'size M hol',
+            'anchor_num' => 0,
+            'panel_size' => 4,
+        ];
+        $response = $this->actingAs($user)->postJson('/api/panels', $changedPreset002panels);
+        $response->assertStatus(Response::HTTP_OK);
+
+        // Preset002Panelsを一部変更して，エラーが発生するようなケースをテスト
+        $errorPreset002Panels = $this->preset002Panels;
+
+        // nullが含まれるケース1
+        $errorPreset002Panels['layout'][1] = [
+            'panel_name' => null,
+            'anchor_num' => 1,
+            'panel_size' => null,
+        ];
+        $response = $this->actingAs($user)->postJson('/api/panels', $errorPreset002Panels);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        // nullが含まれるケース2
+        $errorPreset002Panels['layout'][1] = [
+            'panel_name' => 'size M hol',
+            'anchor_num' => 1,
+            'panel_size' => null,
+        ];
+        $response = $this->actingAs($user)->postJson('/api/panels', $errorPreset002Panels);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        // m_panelに存在しないpanel_nameが含まれるケース
+        $errorPreset002Panels['layout'][1] = [
+            'panel_name' => 'aaaaa',
+            'anchor_num' => 1,
+            'panel_size' => 3,
+        ];
+        $response = $this->actingAs($user)->postJson('/api/panels', $errorPreset002Panels);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        // anchor_numに範囲外の値が設定されているケース
+        $errorPreset002Panels['layout'][1] = [
+            'panel_name' => 'size M hol',
+            'anchor_num' => 16,
+            'panel_size' => 3,
+        ];
+        $response = $this->actingAs($user)->postJson('/api/panels', $errorPreset002Panels);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        // panel_sizeに範囲外の値が設定されているケース
+        $errorPreset002Panels['layout'][1] = [
+            'panel_name' => 'size M hol',
+            'anchor_num' => 1,
+            'panel_size' => 6,
+        ];
+        $response = $this->actingAs($user)->postJson('/api/panels', $errorPreset002Panels);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        // 割り当てができないケース1
+        $response = $this->actingAs($user)->postJson('/api/panels', $this->superimposedPanels);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        // 割り当てができないケース2
+        $errorPreset002Panels = $this->preset002Panels;
+        $errorPreset002Panels['layout'][] = [
+            'panel_name' => 'size S',
+            'anchor_num' => 2,
+            'panel_size' => 5,
+        ];
+        $response = $this->actingAs($user)->postJson('/api/panels', $errorPreset002Panels);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        // dump($errorPreset002Panels);
+        // $response->dump();
     }
 }
